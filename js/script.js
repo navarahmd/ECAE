e69317d8a1d0f222720cb46376f0215b6b34db9d
navarahmd <navarcp@gmail.com>
2025-01-29 04:48:29 +0400

    update

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// SLider
+
+$('.glimpse-row').slick({
+    centerMode: true,
+    centerPadding: '15%',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 2000
+  });
